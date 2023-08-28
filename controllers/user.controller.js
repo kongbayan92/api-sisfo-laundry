37411a9f094e8d437744f75f50a45822f7de7003
Yanzen Project <yanwarsolah@gmail.com>
2023-08-28 11:58:57 +0700

    validasi user register

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,76 +5,65 @@ const userModel = require("../models/user.model");
 const userController = express.Router();
 
 userController.post("/register", async (req, res) => {
-  try {
-    const { nama, email, password } = req.body;
+  // Periksa apakah data lengkap?
+  if (!(req.body.nama && req.body.email && req.body.password)) {
+    return res.status(400).json({ message: "Data harus lengkap." });
+  }
 
-    if (!(nama && password && email)) {
-      return res.status(400).json({ message: "Semua data harus diisi" });
-    }
+  // Periksa apakah user sudah terdaftar?
+  let userAda = await userModel.findOne({ email: req.body.email });
+  if (userAda) {
+    return res.status(400).json({ message: "User telah terdaftar." });
+  }
 
-    const member = await userModel.findOne({ email });
+  // Enkripsi password user
+  const passwordEncrypted = await bcrypt.hash(password, 10);
 
-    if (member) {
-      return res
-        .status(400)
-        .json({ message: "User sudah pernah mendaftar, silahkan sign in." });
-    }
+  // Create user in our database
+  const newUser = await userModel.create({
+    nama,
+    email,
+    password: passwordEncrypted,
+  });
 
-    // Enkripsi password user
-    const passwordEncrypted = await bcrypt.hash(password, 10);
-
-    // Create user in our database
-    const newUser = await userModel.create({
-      nama,
+  // Create JWT Token
+  const token = jwt.sign(
+    {
       email,
-      password: passwordEncrypted,
-    });
+      userId: newUser._id,
+    },
+    process.env.TOKEN_KEY,
+    { expiresIn: "140h" }
+  );
 
+  return res.status(201).json({ token });
+});
+
+userController.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    return res.status(400).json({ message: "Semua data harus diisi" });
+  }
+
+  // validasi jika user ada di dalam database
+  const user = await userModel.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
     // Create JWT Token
     const token = jwt.sign(
       {
         email,
-        userId: newUser._id,
+        userId: user._id,
       },
       process.env.TOKEN_KEY,
       { expiresIn: "140h" }
     );
 
-    return res.status(201).json({ token });
-  } catch (error) {
-    return res.status(400).json({ message: "Ada sesuatu yang bermasalah" });
+    return res.status(200).json({ token });
   }
-});
 
-userController.post("/signin", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    if (!(email && password)) {
-      return res.status(400).json({ message: "Semua data harus diisi" });
-    }
-
-    // validasi jika user ada di dalam database
-    const user = await userModel.findOne({ email });
-
-    if (user && (await bcrypt.compare(password, user.password))) {
-      // Create JWT Token
-      const token = jwt.sign(
-        {
-          email,
-          userId: user._id,
-        },
-        process.env.TOKEN_KEY,
-        { expiresIn: "140h" }
-      );
-
-      return res.status(200).json({ token });
-    }
-
-    return res.status(401).json({ message: "Kredensial tidak valid." });
-  } catch (error) {
-    return res.status(400).json({ message: "Ada sesuatu yang bermasalah" });
-  }
+  return res.status(401).json({ message: "Kredensial tidak valid." });
 });
 
 module.exports = userController;
